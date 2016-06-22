72f216fa1093a1329365f3fdc94614b3ff832aea
Kriti Saxena <krrriti@gmail.com>
2016-06-22 12:57:03 +0530

    updates code for sulpharas new

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -9,25 +9,35 @@ class GildedRose {
 
     public void updateQuality() {
         for (int i = 0; i < items.length; i++) {
+        	System.out.println("name - "+ items[i].name);
+        	//decreases quality
             if (!items[i].name.equals("Aged Brie")
                     && !items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
                 if (items[i].quality > 0) {
                     if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                        items[i].quality = items[i].quality - 1;
+                    		if(!items[i].name.equals("Sulfuras New"))
+                    			items[i].quality = items[i].quality - 1;
+                    		else if(items[i].quality>10)
+                    			items[i].quality = items[i].quality - 1;
                     }
                 }
-            } else {
+            } 
+            // increases quality
+            else {
+            	//The Quality of an item is never more than 50
                 if (items[i].quality < 50) {
                     items[i].quality = items[i].quality + 1;
 
                     if (items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
                         if (items[i].sellIn < 11) {
+                        	//The Quality of an item is never more than 50
                             if (items[i].quality < 50) {
                                 items[i].quality = items[i].quality + 1;
                             }
                         }
 
                         if (items[i].sellIn < 6) {
+                        	//The Quality of an item is never more than 50
                             if (items[i].quality < 50) {
                                 items[i].quality = items[i].quality + 1;
                             }
@@ -35,24 +45,29 @@ class GildedRose {
                     }
                 }
             }
-
+            
+//decreases sellin
             if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
                 items[i].sellIn = items[i].sellIn - 1;
             }
-
+            
+// adjusting quality post sellin : 
             if (items[i].sellIn < 0) {
                 if (!items[i].name.equals("Aged Brie")) {
                     if (!items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
                         if (items[i].quality > 0) {
+                        	//The Quality of an item is never negative
                             if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
                                 items[i].quality = items[i].quality - 1;
                             }
                         }
                     } else {
+                    	//Once the sell by date has passed, Quality degrades twice as fast
                         items[i].quality = items[i].quality - items[i].quality;
                     }
                 } else {
                     if (items[i].quality < 50) {
+                    	//The Quality of an item is never more than 50
                         items[i].quality = items[i].quality + 1;
                     }
                 }
